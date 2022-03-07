ea2461e2db643ef9c521821f1e55e79c0fd2227c
Prathik Reddy <tvkp333555@gmail.com>
2022-03-07 22:53:48 +0530

    creating design of blog and blogpost

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import styles from '../styles/Blog.module.css'
+import Link from "next/link"
 const blog = () => {
   return (
-    <div>This is blog home page</div>
+    <div>
+      <div className={styles.container}>
+        <main className={styles.main}>
+            <div className={styles.blogItem}>
+             <Link href={"/blogpost/learn-javascript"}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>JavaScript is a language used to desing logic for the web</p>
+            </div>
+
+            <div className={styles.blogItem}>
+             <Link href={"/blogpost/learn-javascript"}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>JavaScript is a language used to desing logic for the web</p>
+            </div>
+
+            <div className={styles.blogItem}>
+             <Link href={"/blogpost/learn-javascript"}>
+              <h3>How to learn javascript in 2022</h3></Link>
+              <p>JavaScript is a language used to desing logic for the web</p>
+            </div>
+            
+
+        </main>
+      </div>
+    </div>
   )
 }
 
--- pages/blogpost/[slugPost].js
+++ pages/blogpost/[slugPost].js
@@ -1,9 +1,22 @@
 import { useRouter } from 'next/router';
+import styles from '../../styles/BlogPost.module.css'
 const slugPost = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { slugPost } = router.query
 
-  return <p>Post: {slugPost}</p>
+  return (
+    <>
+      <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page : {slugPost}</h1>
+        <hr />
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id at totam consequatur nemo quidem quas suscipit architecto alias similique, ad autem odio sunt aut saepe! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem asperiores quis voluptates praesentium delectus eveniet, minus inventore animi? Minus nulla dicta itaque dolor fuga delectus, deleniti veritatis, possimus saepe quia numquam ad modi doloribus illum.</p>
+        </div>
+      </main>
+      </div>
+    </>
+  )
 }
 
 export default slugPost
