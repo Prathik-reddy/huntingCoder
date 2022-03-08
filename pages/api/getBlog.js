b471739d8590c58749bf015e52de034d170ec773
Prathik Reddy <tvkp333555@gmail.com>
2022-03-08 18:27:21 +0530

    reading files using slug

---
+++ pages/api/getBlog.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+//http://localhost:3000/api/getBlog?slug=howToLearnFlask
+export default function handler(req, res) {
+  fs.readFile(`blogData/${req.query.slug}.json`,"utf-8",(err,data) => {
+    if(err){
+    res.status(500).json({err:"No Such Blog Found"});
+    }
+    console.log(req.query.slug);
+    res.status(200).json(JSON.parse(data));
+  })
+}
