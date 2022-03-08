d980d4b8430adeb84e3905b144b9bbc5056aaf01
Prathik Reddy <tvkp333555@gmail.com>
2022-03-08 20:23:31 +0530

    using api's to get data

--- pages/api/Blogs.js
+++ pages/api/Blogs.js
@@ -1,7 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
-export default function handler(req, res) {
-  fs.readdir("blogData",(err,data) => {
-    res.status(200).json(data);
-  })
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogData");
+  let myfile;
+  let allBlogs=[]
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    myfile = await fs.promises.readFile(("blogData/" + item),"utf-8");
+    console.log(myfile);
+    allBlogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json(allBlogs);
 }
--- pages/blog.js
+++ pages/blog.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from "next/link"
 const blog = () => {
+  const [blog, setblog] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/api/Blogs').then((a) => {
+      return a.json();
+    })
+      .then((parsed) => {
+        console.log(parsed);
+        setblog(parsed);
+      })
+  }, [])
   return (
     <div>
       <div className={styles.container}>
         <main className={styles.main}>
-            <div className={styles.blogItem}>
-             <Link href={"/blogpost/learn-javascript"}>
-              <h3>How to learn javascript in 2022</h3></Link>
-              <p>JavaScript is a language used to desing logic for the web</p>
-            </div>
 
-            <div className={styles.blogItem}>
-             <Link href={"/blogpost/learn-javascript"}>
-              <h3>How to learn javascript in 2022</h3></Link>
-              <p>JavaScript is a language used to desing logic for the web</p>
-            </div>
-
-            <div className={styles.blogItem}>
-             <Link href={"/blogpost/learn-javascript"}>
-              <h3>How to learn javascript in 2022</h3></Link>
-              <p>JavaScript is a language used to desing logic for the web</p>
-            </div>
-            
-
+          {blog.map((blogItem) => {
+            return (
+              <div className={styles.blogItem} key={blogItem.slug}>
+                <Link href={`/blogpost/${blogItem.slug}`}>
+                  <h3>{blogItem.title}</h3></Link>
+                  <p>{blogItem.content.substr(0,150)}...</p>
+              </div>
+            )
+          })
+          }
         </main>
       </div>
     </div>
