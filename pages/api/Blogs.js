c150f80980e1809cf8d50ba523535662f0920a80
Prathik Reddy <tvkp333555@gmail.com>
2022-03-29 20:29:10 +0530

    adding infinite scroll

--- pages/api/Blogs.js
+++ pages/api/Blogs.js
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from 'fs';
 export default async function handler(req, res) {
+  console.log(req.query.count);
   let data = await fs.promises.readdir("blogData");
+  data = data.slice(0,parseInt(req.query.count));
   let myfile;
   let allBlogs=[]
   for (let i = 0; i < data.length; i++) {
--- pages/blog.js
+++ pages/blog.js
@@ -1,43 +1,67 @@
 import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from "next/link"
+import InfiniteScroll from 'react-infinite-scroll-component';
 import * as fs from 'fs';
 const Blog = (props) => {
   console.log(props);
   const [blog, setblog] = useState(props.allBlogs);
+  const [count, setcount] = useState(2);
+  const fetchMoreData = async () => {
+    let d = await fetch(`http://localhost:3000/api/Blogs/?count=${count+2}`);
+    setcount(count+2);
+    let data = await d.json();
+    setblog(data);
+
+  };
+
+
   return (
     <div>
       <div className={styles.container}>
         <main className={styles.main}>
 
-          {blog.map((blogItem) => {
-            return (
-              <div className={styles.blogItem} key={blogItem.slug}>
-                <Link href={`/blogpost/${blogItem.slug}`}>
-                  <h3>{blogItem.title}</h3></Link>
-                  <p>{blogItem.metadesc.substr(0,150)}...</p>
+
+          <InfiniteScroll
+            dataLength={blog.length} //This is important field to render the next data
+            next={fetchMoreData}
+            hasMore={props.allCount!==blog.length}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+              <p style={{ textAlign: 'center' }}>
+                <b>Yay! You have seen it all</b>
+              </p>
+            }>
+            {blog.map((blogItem) => {
+              return (
+                <div className={styles.blogItem} key={blogItem.slug}>
+                  <Link href={`/blogpost/${blogItem.slug}`}>
+                    <h3>{blogItem.title}</h3></Link>
+                  <p>{blogItem.metadesc.substr(0, 150)}...</p>
                   <hr />
-              </div>
-            )
-          })
-          }
+                </div>
+              )
+            })
+            }
+          </InfiniteScroll>
         </main>
       </div>
-    </div>
+    </div >
   )
 };
 export async function getStaticProps(context) {
   let data = await fs.promises.readdir("blogData");
+  let allCount = data.length;
   let myfile;
   let allBlogs = [];
-    for (let index = 0; index < data.length; index++) {
-      const item = data[index];
-        console.log(item)
-        myfile = await fs.promises.readFile(('blogData/' + item), 'utf-8')
-        allBlogs.push(JSON.parse(myfile))
-    }
-    return {
-      props: {allBlogs},
-}
+  for (let index = 0; index < 2; index++) {
+    const item = data[index];
+    console.log(item)
+    myfile = await fs.promises.readFile(('blogData/' + item), 'utf-8')
+    allBlogs.push(JSON.parse(myfile))
+  }
+  return {
+    props: { allBlogs ,allCount },
+  }
 }
 export default Blog
